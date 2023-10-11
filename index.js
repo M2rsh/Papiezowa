82478eb6bfd66f2d7f46b05fb01c9934fb0a38a7
M2rsh <mariuszbubak10@gmail.com>
2023-10-11 20:12:59 +0200

    Holy shit i actually fixed it

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-const countdown = document.getElementById('countdown');
-countdown.innerText = "Ładowanie..."
+const countdown = document.getElementById("countdown");
+countdown.innerText = "Ładowanie...";
 
 let completeDate = new Date();
-completeDate.setHours(21, 37, 00)
-
+completeDate.setHours(21, 37, 0o0);
 
 let colours = [];
 for (let i = 0; i < 100; i++) {
-	colours.push("hsl(" + (360 * i / 100) + ",80%,50%)");
+  colours.push("hsl(" + (360 * i) / 100 + ",80%,50%)");
 }
 
-var _int1 = setInterval(function() {
-	const currentDate = new Date()
-	var remaining = completeDate - currentDate;
-	if(remaining < 0) {
-		remaining = completeDate - currentDate.setDate(currentDate.getDate()-1);
-	}
-	const days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+var _int1 = setInterval(function () {
+  const currentDate = new Date();
+  var remaining = completeDate - currentDate;
+  if (remaining < 0) {
+    remaining = completeDate - currentDate.setDate(currentDate.getDate() - 1);
+  }
+  const days = Math.floor(remaining / (1000 * 60 * 60 * 24));
   const hours = Math.floor(remaining / (1000 * 60 * 60)) % 24;
   const minutes = Math.floor(remaining / (1000 * 60)) % 60;
   const seconds = Math.floor(remaining / 1000) % 60;
 
-	if ( (days+hours+minutes+seconds)<=0 ) {
-		afterCountdown();
-		clearInterval(_int1);
-	} else {
-		countdown.innerText = `Zostało ${days!=0 ? days+' days, ' : ""}${hours} ${hours <= 1 ? "Godziny" : "Godzina"} ${minutes} Minut ${seconds} Sekund.`;
-	}
-}, 1000)
+  if (days + hours + minutes + seconds <= 0) {
+    afterCountdown();
+    clearInterval(_int1);
+  } else {
+    countdown.innerText = `${hours}h ${minutes}min ${seconds}s`;
+  }
+}, 1000);
 
 function afterCountdown() {
-		countdown.innerText="PAPIErzOWA!";
-		setInterval(() => (confetti()), 1000);
-		i = 0;
-		setInterval(function() {
-			countdown.style.color = colours[i];
-			i++
-			if(i==101){
-				i=0;
-			}
-	},10)
+  countdown.innerText = "PAPIEŻOWA!";
+  setInterval(() => confetti(), 1000);
+  i = 0;
+  setInterval(function () {
+    countdown.style.color = colours[i];
+    i++;
+    if (i == 101) {
+      i = 0;
+    }
+  }, 10);
+  setTimeout(function () {
+    location.reload();
+  }, 60000);
 }
 
-console.log("The page is not real")
-
+console.log("The page is not real");
